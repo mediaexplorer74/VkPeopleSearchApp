f2ff337647a00b575e8b4b65a4cfd38bc5230af8
Andrey Zakharov <mediaexplorer74@hotmail.com>
2022-04-10 10:43:41 +0300

    .

---
+++ README.md
@@ -0,0 +1,15 @@
+# VkPeopleSearchApp
+A sample Windows 10 Mobile app for updated VkLib library. 
+
+This is the mini-research to explore how to use VkLib to perform OAuth and to search users in VK.com.
+
+The code based on VkLib by Stealth2012
+
+Status:
+work-in-progress
+
+AS IS. No support. RnD only.
+
+-- mediaexplorer 2022
+
+
---
+++ VkPeopleSearchApp/App.xaml.cs
@@ -0,0 +1,248 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace VkPeopleSearchApp
+{
+    /// <summary>
+    /// Обеспечивает зависящее от конкретного приложения поведение, дополняющее класс Application по умолчанию.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Инициализирует одноэлементный объект приложения. Это первая выполняемая строка разрабатываемого
+        /// кода, поэтому она является логическим эквивалентом main() или WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Вызывается при обычном запуске приложения пользователем. Будут использоваться другие точки входа,
+        /// например, если приложение запускается для открытия конкретного файла.
+        /// </summary>
+        /// <param name="e">Сведения о запросе и обработке запуска.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Не повторяйте инициализацию приложения, если в окне уже имеется содержимое,
+            // только обеспечьте активность окна
+            if (rootFrame == null)
+            {
+                // Создание фрейма, который станет контекстом навигации, и переход к первой странице
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Загрузить состояние из ранее приостановленного приложения
+                }
+
+                // Размещение фрейма в текущем окне
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // Если стек навигации не восстанавливается для перехода к первой странице,
+                    // настройка новой страницы путем передачи необходимой информации в качестве параметра
+                    // навигации
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Обеспечение активности текущего окна
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// Вызывается в случае сбоя навигации на определенную страницу
+        /// </summary>
+        /// <param name="sender">Фрейм, для которого произошел сбой навигации</param>
+        /// <param name="e">Сведения о сбое навигации</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Вызывается при приостановке выполнения приложения.  Состояние приложения сохраняется
+        /// без учета информации о том, будет ли оно завершено или возобновлено с неизменным
+        /// содержимым памяти.
+        /// </summary>
+        /// <param name="sender">Источник запроса приостановки.</param>
+        /// <param name="e">Сведения о запросе приостановки.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Сохранить состояние приложения и остановить все фоновые операции
+            deferral.Complete();
+        }
+    }
+}
+*/
+
+using System;
+using System.Diagnostics;
+
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+using VkPeopleSearchApp.Domain;
+using VkPeopleSearchApp.Services;
+using VkPeopleSearchApp.View;
+
+using GalaSoft.MvvmLight.Threading;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace VkPeopleSearchApp
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App : Application
+    {
+        private TransitionCollection _transitions;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+#if DEBUG
+            if (Debugger.IsAttached)
+            {
+                //DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+            ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseCoreWindow);
+
+            Settings.Instance.Load();
+
+            ServiceLocator.Vkontakte.AccessToken = Settings.Instance.AccessToken;
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                // TODO: change this value to a cache size that is appropriate for your application
+                rootFrame.CacheSize = 1;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // Removes the turnstile navigation for startup.
+                if (rootFrame.ContentTransitions != null)
+                {
+                    _transitions = new TransitionCollection();
+                    foreach (var c in rootFrame.ContentTransitions)
+                    {
+                        _transitions.Add(c);
+                    }
+                }
+
+                rootFrame.ContentTransitions = null;
+                rootFrame.Navigated += RootFrame_FirstNavigated;
+
+                Type startPage = null;
+                if (Settings.Instance.AccessToken != null && !Settings.Instance.AccessToken.HasExpired)
+                    startPage = typeof(MainPage);
+                else
+                    startPage = typeof(LoginView);
+
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(startPage, e.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+
+            DispatcherHelper.Initialize();
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Restores the content transitions after the app has launched.
+        /// </summary>
+        /// <param name="sender">The object where the handler is attached.</param>
+        /// <param name="e">Details about the navigation event.</param>
+        private void RootFrame_FirstNavigated(object sender, NavigationEventArgs e)
+        {
+            var rootFrame = sender as Frame;
+            rootFrame.ContentTransitions = _transitions ?? new TransitionCollection { new NavigationThemeTransition() };
+            rootFrame.Navigated -= RootFrame_FirstNavigated;
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            Settings.Instance.Save();
+
+            var deferral = e.SuspendingOperation.GetDeferral();
+
+            // TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
